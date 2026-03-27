ec44d0d0ab9b5e74831ddfd92ee4ab1182598428
SAGER_1902 <1441635519@qq.com>
2026-03-27 04:39:59 +0000

    删除文件 src/main

--- src/main/java/com/wmlib/api/IPara.java
+++
@@ -1,7 +0,0 @@
-package wmlib.api;
-import net.minecraft.world.entity.LivingEntity;
-public interface IPara
-{
-	public void setDrop();
-	public boolean isDrop();
-}
--- src/main/java/com/wmlib/common/enchantment/EnchantmentTypes.java
+++
@@ -1,8 +0,0 @@
-package wmlib.common.enchantment;
-import wmlib.common.item.ItemBless;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
-public class EnchantmentTypes
-{
-    public static final EnchantmentCategory VEHICLE = EnchantmentCategory.create("wmlib:vehicle", item -> item instanceof ItemBless);
-	public static final EnchantmentCategory CREATURE = EnchantmentCategory.create("wmlib:creature", item -> item instanceof ItemBless);
-}
